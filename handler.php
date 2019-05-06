684cb6302374e698ba68bfcf7725a10df6063a6c
1llum1nati <43194859+1llum1nati@users.noreply.github.com>
2019-05-06 06:58:59 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,11 +1,31 @@
+
 <?php
-$email = $_POST['email'];
-$email = htmlspecialchars($email);
-$email = urldecode($email);
-$email = trim($email);
-//echo $email;
-if (mail("appdevice@yandex.ru", "Заявка с сайта", "E-mail:".$email. ,"From \r\n"))
- {     echo "сообщение успешно отправлено"; 
-} else { 
-    echo "при отправке сообщения возникли ошибки";
-}?>
+ 
+$message = "Имя: {$_POST['name']}<br>"; // добавляем имя в текст
+$message .= "Телефон: {$_POST['phone']}"; // добавляем телефон в текст
+ 
+require 'phpmailer/PHPMailerAutoload.php';
+ 
+$mail = new PHPMailer;
+$mail->isSMTP();
+ $email = $_POST['email'];
+$mail->Host = 'smtp.mail.ru';
+$mail->SMTPAuth = true;
+$mail->Username = 'appdevice@yandex.ru'; // логин от вашей почты
+$mail->Password = 'eA#VjE9BNS'; // пароль от почтового ящика
+$mail->SMTPSecure = 'ssl';
+$mail->Port = '465';
+$mail->CharSet = 'UTF-8';
+$mail->From = 'mailer_test_2014@mail.ru';
+$mail->FromName = 'Андрей';
+$mail->addAddress($email, 'Андрей');
+$mail->isHTML(true);
+$mail->Subject = 'Заказ обратного звонка';
+$mail->Body = $message;
+ 
+if( $mail->send() ){
+ echo '<p style="color: green;">Ваше сообщение отправлено</p>';
+}else{
+ echo '<p style="color: red;">Ошибка!</p>';
+}
+?>
