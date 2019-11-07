6779e669c1436916e9ec8644637aeac2df181ea3
1llum1nati <43194859+1llum1nati@users.noreply.github.com>
2019-11-08 00:03:30 +0300

    Update Program.cs

--- PipeC#/Client/polygon2/Program.cs
+++ PipeC#/Client/polygon2/Program.cs
@@ -10,16 +10,9 @@ namespace PipeApplication1
 
         public void ThreadStartClient(object obj)
         {
-            // Ensure that we only start the client after the server has created the pipe
             ManualResetEvent SyncClientServer = (ManualResetEvent)obj;
-
-            // Only continue after the server was created -- otherwise we just fail badly
-            // SyncClientServer.WaitOne();
-
             using (NamedPipeClientStream pipeStream = new NamedPipeClientStream("mytestpipe"))
             {
-                // The connect function will indefinately wait for the pipe to become available
-                // If that is not acceptable specify a maximum waiting time (in ms)
                 pipeStream.Connect();
                 FileStream file = new FileStream("/home/r3pl1c4nt/Projects/2.txt", FileMode.Open);
                 Console.WriteLine("[Client] Pipe connection established");
@@ -35,10 +28,6 @@ namespace PipeApplication1
 
         static void Main(string[] args)
         {
-
-            // To simplify debugging we are going to create just one process, and have two tasks
-            // talk to each other. (Which is a bit like me sending an e-mail to my co-workers)
-
             ProgramPipeTest Client = new ProgramPipeTest();
 
             Thread ClientThread = new Thread(Client.ThreadStartClient);
